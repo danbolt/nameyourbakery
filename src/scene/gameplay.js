3fc78edbf5fc896b321026fa63edc43be3206f0a
Daniel Savage <dansvg@gmail.com>
2021-06-04 19:58:58 -0700

    omg yess tiles

--- src/scene/gameplay.js
+++ src/scene/gameplay.js
@@ -5,6 +5,16 @@ export const scene = function () {
 scene.prototype.init = function () {
 	//
 };
+scene.prototype.preload = function () {
+	this.load.image('tiles', 'asset/image/tiles.png');
+
+	this.load.tilemapTiledJSON('level0', 'asset/map/level0.json');
+};
+scene.prototype.create = function () {
+	const map = this.make.tilemap({ key: 'level0' });
+	const tiles = map.addTilesetImage('tiles', 'tiles');
+	const foreground = map.createLayer('foreground', tiles);
+}
 scene.prototype.update = function () {
 	// console.log('bing')
 };
